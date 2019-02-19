e10735e9520b336c8ee2273cac27081193e59afd
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2019-02-19 17:02:23 +0530

    Added release version

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import * as data from '../../../../git-version.json';
 @Component({
   selector: 'app-footer',
   templateUrl: 'footer.component.html'
 })
 
-export class FooterComponent { }
+export class FooterComponent {
+  public git = data['default'];
+}
