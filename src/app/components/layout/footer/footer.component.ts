676dd5cb9df863c7cc32186cdfa2826969e16e9d
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2019-02-20 13:31:59 +0530

    fixed per feedback

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import * as data from '../../../../git-version.json';
+import { version as appVersion } from '../../../../../package.json';
+
 @Component({
   selector: 'app-footer',
   templateUrl: 'footer.component.html'
 })
 
 export class FooterComponent {
+  public appVersion: string = appVersion;
   public git = data['default'];
 }
