358aa517241ce3d6076e7b6738e8a222f36599fb
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2019-06-14 17:03:48 +0530

    install plugin on rule and deliver notification page

--- src/app/components/core/notifications/add-notification-wizard/add-notification-wizard.component.ts
+++ src/app/components/core/notifications/add-notification-wizard/add-notification-wizard.component.ts
@@ -30,7 +30,6 @@ export class AddNotificationWizardComponent implements OnInit {
   public isNotificationEnabled = true;
 
   public payload: any = {};
-
   public rulePluginConfigurationData: any;
   public rulePluginChangedConfig: any;
 
@@ -53,6 +52,12 @@ export class AddNotificationWizardComponent implements OnInit {
 
   @ViewChild(ViewConfigItemComponent) viewConfigItemComponent: ViewConfigItemComponent;
 
+  public pluginData = {
+    modalState: false,
+    type: '',
+    pluginName: ''
+  };
+
   constructor(private formBuilder: FormBuilder,
     private notificationService: NotificationsService,
     private alertService: AlertService,
@@ -69,6 +74,17 @@ export class AddNotificationWizardComponent implements OnInit {
       rule: ['', Validators.required],
       delivery: ['', Validators.required]
     });
+  }
+
+  /**
+   * Open plugin modal
+   */
+  openPluginModal(state: boolean, pluginType: string) {
+    this.pluginData = {
+      modalState: state,
+      type: pluginType,
+      pluginName: ''
+    };
   }
 
   getNotificationPlugins() {
@@ -93,6 +109,14 @@ export class AddNotificationWizardComponent implements OnInit {
         } else {
           this.alertService.error(error.statusText);
         }
+      },
+      () => {
+        setTimeout(() => {
+          if (this.pluginData.modalState) {
+            this.pluginData.modalState = false;
+            this.selectInstalledPlugin();
+          }
+        }, 1000);
       });
   }
 
@@ -187,7 +211,6 @@ export class AddNotificationWizardComponent implements OnInit {
           this.isRulePlugin = false;
           return;
         }
-
         if (formValues['rule'].length !== 1) {
           this.isSinglePlugin = false;
           this.selectedRulePluginDescription = '';
@@ -474,5 +497,31 @@ export class AddNotificationWizardComponent implements OnInit {
             this.alertService.error(error.statusText);
           }
         });
+  }
+
+  onNotify(event: any) {
+    this.pluginData.modalState = event.modalState;
+    this.pluginData.pluginName = event.name;
+    this.pluginData.type = event.type;
+    if (event.modalState) {
+      this.getNotificationPlugins();
+    }
+  }
+
+  selectInstalledPlugin() {
+    const select = <HTMLSelectElement>document.getElementById(this.pluginData.type.toLowerCase());
+    for (let i = 0, j = select.options.length; i < j; ++i) {
+      if (select.options[i].innerText.toLowerCase() === this.pluginData.pluginName.toLowerCase()) {
+        if (this.pluginData.type.toLowerCase() === 'rule') {
+          this.notificationForm.controls['rule'].setValue([this.notificationRulePlugins[i].name]);
+        } else if (this.pluginData.type.toLowerCase() === 'notify') {
+          this.notificationForm.controls['delivery'].setValue([this.notificationDeliveryPlugins[i].name]);
+        }
+        const nxtButton = <HTMLButtonElement>document.getElementById('next');
+        nxtButton.disabled = false;
+        select.selectedIndex = i;
+        break;
+      }
+    }
   }
 }
--- src/app/components/core/plugin-modal/plugin-modal.component.ts
+++ src/app/components/core/plugin-modal/plugin-modal.component.ts
@@ -46,7 +46,8 @@ export class PluginModalComponent implements OnInit, OnChanges {
     }
     this.notify.emit({
       modalState: false,
-      name: ''
+      name: '',
+      type: this.data.type.toLowerCase()
     });
     modal.classList.remove('is-active');
   }
@@ -94,6 +95,9 @@ export class PluginModalComponent implements OnInit, OnChanges {
   }
 
   installPlugin(pluginName: string) {
+    if (pluginName === undefined) {
+      return;
+    }
     const pluginData = {
       format: 'repository',
       name: `foglamp-${this.data.type.toLowerCase()}-` + pluginName,
@@ -124,6 +128,7 @@ export class PluginModalComponent implements OnInit, OnChanges {
         () => {
           this.notify.emit({
             modalState: true,
+            type: this.data.type.toLowerCase(),
             name: pluginName
           });
         });
